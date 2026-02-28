7fcdc9e36d6a462877c3c5039025b1976f5f41c5
SusamaDutta <duttasusama010@gmail.com>
2026-02-28 23:43:06 +0530

    Create Str3.java

---
+++ Str3.java
@@ -0,0 +1,58 @@
+/*  
+String myString = "Hello World!"
+The elements of a String are called characters. The number of characters in a String is called the length, and it can be retrieved with the String.length() method.
+
+Given two strings of lowercase English letters, A and B, perform the following operations:
+1.Sum the lengths of A and B.
+2.Determine if A is lexicographically larger than B (i.e.: does B come before A in the dictionary?).
+3.Capitalize the first letter in A and B and print them on a single line, separated by a space.
+
+Input Format : The first line contains a string A. The second line contains another string B. The strings are comprised of only lowercase English letters.
+
+Output Format : There are three lines of output:
+For the first line, sum the lengths of A and B.
+For the second line, write Yes if A is lexicographically greater than B otherwise print No instead.
+For the third line, capitalize the first letter in both A and B and print them on a single line, separated by a space.
+
+Sample Input : 
+hello
+java
+
+Sample Output :
+9
+No
+Hello Java
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        
+        int A1=A.length();
+        int B1=B.length();
+        
+        int c=A1+B1;
+        System.out.println(c);
+        
+        if(A.compareTo(B)>0){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+        
+        System.out.print(A.substring(0,1).toUpperCase()+A.substring(1)+" "+B.substring(0,1).toUpperCase()+B.substring(1));
+    }
+}
+
+
+
+
